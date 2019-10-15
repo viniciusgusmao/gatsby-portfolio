7290703e43187bea24f2c9976eda3cde0bede5a0
Vinicius <vinicius-og@hotmail.com>
2019-10-15 01:47:47 -0300

    Imagem background, overlay da cor preta

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,46 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import backgroundImg from "../images/background-note.jpg";
+import styled from "styled-components";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const Header = () => (  
+    <ContainerBlack>
+      <ContainerImg>      
+        <Container>
+          <Titulo>Vinícius Gusmão</Titulo>
+        </Container>
+      </ContainerImg>
+    </ContainerBlack>
 )
+
+const Titulo = styled.h1`
+  color: white;
+  font-size: 4em;
+`;
+
+const ContainerBlack = styled.div`
+  background-color: black;
+  opacity: 0.9;
+`;
+
+const ContainerImg = styled.div`
+   width: '100%';
+   height: ${window.innerHeight+'px'};
+   background-image: url(${backgroundImg});    
+   background-size: 'constrained';
+   background-position: center;      
+   
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;  
+`;
+
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header />
       <div
         style={{
           margin: `0 auto`,
@@ -36,7 +36,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Construído com
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
